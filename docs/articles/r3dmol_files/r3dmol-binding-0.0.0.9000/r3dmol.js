3adbc750216d60d0489bd3c08f754c96df95bb31
swsoyee <swsoyee@gmail.com>
2020-09-04 20:24:54 +0900

    add: use pkgdown

---
+++ docs/articles/r3dmol_files/r3dmol-binding-0.0.0.9000/r3dmol.js
@@ -0,0 +1,38 @@
+HTMLWidgets.widget({
+
+  name: 'r3dmol',
+
+  type: 'output',
+
+  factory: function (el, width, height) {
+
+    // TODO: define shared variables for this instance
+
+    return {
+
+      renderValue: function (x) {
+        $(el).empty();
+        $(el).css({
+          width: x.width || "100%",
+          height: x.height || "500px",
+          position: x.position || "relative",
+        })
+        const config = {
+          backgroundColor: 'white',
+        };
+        let v = $3Dmol.createViewer($(el), config);
+        v.addModel(x.data.join("\n"), "pdb");
+        v.setStyle({}, { cartoon: { color: 'spectrum' } });
+        v.zoomTo();
+        v.render();
+      },
+
+      resize: function (width, height) {
+
+        // TODO: code to re-render the widget with a new size
+
+      }
+
+    };
+  }
+});
