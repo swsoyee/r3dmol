e0ac324d170d1d574c1ba41676eb58daeba424d4
swsoyee <swsoyee@gmail.com>
2020-09-04 23:57:12 +0900

    refactor: use initial value for htmlwidgets

--- inst/htmlwidgets/r3dmol.js
+++ inst/htmlwidgets/r3dmol.js
@@ -6,25 +6,29 @@ HTMLWidgets.widget({
 
   factory: (el, width, height) => {
 
-    // TODO: define shared variables for this instance
+    let initialized = false;
 
     return {
 
       renderValue: x => {
-        $(el).empty();
-        $(el).css({
-          width: x.width || "100%",
-          height: x.height || "500px",
-          position: x.position || "relative",
-        })
-        const config = {
-          backgroundColor: 'white',
-        };
-        let v = $3Dmol.createViewer($(el), config);
-        v.addModel(x.data.join("\n"), "pdb");
-        v.setStyle({}, { cartoon: { color: 'spectrum' } });
-        v.zoomTo();
-        v.render();
+        if (!initialized) {
+          initialized = true;
+
+          $(el).empty();
+          $(el).css({
+            width: x.width || "100%",
+            height: x.height || "500px",
+            position: x.position || "relative",
+          })
+          const config = {
+            backgroundColor: 'white',
+          };
+          let v = $3Dmol.createViewer($(el), config);
+          v.addModel(x.data.join("\n"), "pdb");
+          // v.setStyle({}, { cartoon: { color: 'spectrum' } });
+          v.zoomTo();
+          v.render();
+        }
       },
 
       resize: (width, height) => {
