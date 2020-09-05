875926090aae1bdfedd47a481369f51900c9d8b8
swsoyee <swsoyee@gmail.com>
2020-09-05 14:58:45 +0900

    chore: move general function into utils

---
+++ R/utils.R
@@ -0,0 +1,39 @@
+callJS <- function() {
+  # get the parameters from the function that have a value
+  message <- Filter(function(x) !is.symbol(x), as.list(parent.frame(1)))
+  session <- shiny::getDefaultReactiveDomain()
+
+  # If a r3dmol widget was passed in, this is during a chain pipeline in the
+  # initialization of the widget, so keep track of the desired function call
+  # by adding it to a list of functions that should be performed when the widget
+  # is ready
+  if (methods::is(message$id, "r3dmol")) {
+    widget <- message$id
+    message$id <- NULL
+    widget$x$api <- c(widget$x$api, list(message))
+    return(widget)
+  }
+  # If an ID was passed, the widget already exists and we can simply call the
+  # appropriate JS function
+  else if (is.character(message$id)) {
+    message$id <- session$ns(message$id)
+    method <- paste0("r3dmol:", message$method)
+    session$sendCustomMessage(method, message)
+    return(message$id)
+  } else {
+    stop("The `id` argument must be either a r3dmol htmlwidget or an ID
+         of a r3dmol htmlwidget.", call. = FALSE)
+  }
+}
+
+#' Pipe operator
+#'
+#' See \link[=magrittr]{\%>\%} for details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
