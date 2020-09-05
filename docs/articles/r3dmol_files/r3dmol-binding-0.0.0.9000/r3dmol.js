1159bf02a795270f55d63d64f9dc9b4b4a25e219
swsoyee <swsoyee@gmail.com>
2020-09-05 13:21:53 +0900

    update: pkgdown docs

--- docs/articles/r3dmol_files/r3dmol-binding-0.0.0.9000/r3dmol.js
+++ docs/articles/r3dmol_files/r3dmol-binding-0.0.0.9000/r3dmol.js
@@ -4,35 +4,92 @@ HTMLWidgets.widget({
 
   type: 'output',
 
-  factory: function (el, width, height) {
+  factory: (el, width, height) => {
 
-    // TODO: define shared variables for this instance
+    let initialized = false;
+    const elementId = el.id;
+    const container = document.getElementById(elementId);
+    let view = $3Dmol.createViewer($(container), { backgroundColor: 'white' });
 
     return {
 
       renderValue: function (x) {
-        $(el).empty();
-        $(el).css({
-          width: x.width || "100%",
-          height: x.height || "500px",
-          position: x.position || "relative",
-        })
-        const config = {
-          backgroundColor: 'white',
-        };
-        let v = $3Dmol.createViewer($(el), config);
-        v.addModel(x.data.join("\n"), "pdb");
-        v.setStyle({}, { cartoon: { color: 'spectrum' } });
-        v.zoomTo();
-        v.render();
+        // alias this
+        const that = this;
+        if (!initialized) {
+          initialized = true;
+          // attach the widget to the DOM
+          container.widget = that;
+
+          $(el).css({
+            width: x.width || "100%",
+            height: x.height || "500px",
+            position: x.position || "relative",
+          })
+          view.addModel(x.data.join("\n"), "pdb");
+          view.zoomTo();
+          view.render();
+        }
+
+        // Now that the widget is initialized, call any outstanding API
+        // functions that the user wantd to run on the widget
+        const numApiCalls = x['api'].length;
+        console.log(x);
+        for (let i = 0; i < numApiCalls; i++) {
+          let call = x['api'][i];
+          const method = call.method;
+          delete call['method'];
+          try {
+            that[method](call);
+          } catch (err) { }
+        }
       },
 
-      resize: function (width, height) {
+      resize: (width, height) => {
 
         // TODO: code to re-render the widget with a new size
 
-      }
+      },
 
+      addStyle: ({ }) => {
+        view.addStyle(params.sel, params.style);
+        view.render();
+      },
+
+      setStyle: (params) => {
+        view.setStyle(params.sel, params.style);
+        view.render();
+      },
+
+      spin: (params) => {
+        view.spin(params.axis);
+        view.render();
+      },
+
+      setZoomLimits: (params) => {
+        view.setZoomLimits(params.lower, params.upper);
+        view.render();
+      },
+
+      stopAnimate: ({ }) => { // TODO: not working.
+        view.stopAnimate();
+        view.render();
+      },
+
+      translate: (params) => {
+        view.translate(params.x, params.y, params.animationDuration, params.fixedPath);
+        view.render();
+      },
+
+      translateScene: (params) => { // TODO: need test.
+        view.translateScene(params.x, params.y, params.animationDuration, params.fixedPath);
+        view.render();
+      },
+
+      zoom: (params) => {
+        view.zoom(params.factor, params.animationDuration, params.fixedPath);
+        view.render();
+      },
     };
   }
 });
