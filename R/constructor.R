8890e182d13f9f0c76772129d2f85f899871b987
swsoyee <swsoyee@gmail.com>
2020-09-06 13:54:56 +0900

    add: m_vector3 function

---
+++ R/constructor.R
@@ -0,0 +1,31 @@
+#' Create a 3 dimensional vector
+#'
+#' @param x x coordinate, \code{character} and \code{numeric} are both accepted.
+#' @param y y coordinate, \code{character} and \code{numeric} are both accepted.
+#' @param z z coordinate, \code{character} and \code{numeric} are both accepted.
+#'
+#' @return 3 dimensional list object
+#' @export
+#'
+#' @examples
+#' library(r3dmol)
+#' m_vector3(1, 2, 3)
+m_vector3 <- function(x = NULL,
+                      y = NULL,
+                      z = NULL) {
+  if (missing(x) || missing(y) || missing(z))
+    stop("3 dimensional should by passed.")
+
+  vector_char <- as.character(c(x, y, z))
+  vector_number <- tryCatch({
+    as.numeric(vector_char)
+  }, warning = function(w) {
+    stop("Dimensional is not number.")
+  }, error = function(e) {
+    stop("Dimensional format error.")
+  })
+
+  return(list(x = vector_number[1],
+              y = vector_number[2],
+              z = vector_number[3]))
+}
