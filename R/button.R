8b5c49c847ace5f308046276f2759ae8a284fa2f
Brady <brady.johnston@me.com>
2021-08-13 15:28:01 +0800

    created some default button functions that can take existing m_sel() and m_style_*() functions. Still very rough and require complete refactor and documentation before they are usable.

--- R/button.R
+++ R/button.R
@@ -66,3 +66,136 @@ m_button <- function(id,
   method <- "button"
   callJS()
 }
+
+
+
+##### Test functions for making custom buttons
+library(r3dmol)
+
+
+to_selection <- function(sel) {
+  string <- gsub("\\\"", "", sel)
+  string <- gsub("(?=\\d+)", "\"", string)
+  string
+}
+
+rjson::toJSON(m_sel(resi = c(1,10))) %>%
+  to_selection()
+
+m_sel(resi = c(1,10), chain = "A", resn = c("ALA", "GLY")) %>% rjson::toJSON()
+
+m_2json <- function(sel) {
+
+  print(names(sel))
+  unlist(sel)
+  paste0(
+    "{", names(),"}"
+  )
+}
+
+
+
+m_button_style <- function(id, style = m_style_cartoon(), sel = m_sel(), label = "style") {
+
+
+
+  m_button(
+    id = id,
+    name = "cartoon",
+    label = label,
+    align_items = "flex-end",
+    justify_content = "center",
+    func = paste0("
+        function() {
+          viewer.addStyle(", rjson::toJSON(sel), ",", rjson::toJSON(style), ");
+          viewer.render();
+        }
+      "
+    )
+  )
+}
+
+m_button_style2 <- function(id){
+  m_button(
+    id = id,
+    name = "stick",
+    label = "Stick",
+    func = "
+      function() {
+        viewer.setStyle({stick:{}});
+        viewer.render();
+      }
+    "
+  )
+}
+
+m_button_spin <- function(id, speed = 1, axis = "y", label = "Spin"){
+  m_button(
+    id = id,
+    name = "spin",
+    label = label,
+    func = paste0("
+        function() {
+          viewer.spin(\"", axis, "\",", speed, ");
+          viewer.render();
+      }
+      ")
+  )
+}
+
+m_button_stop_spin <- function(id, label = "Stop!") {
+  m_button(
+    id = id,
+    name = "stopspin",
+    label = "Stop!",
+    func = paste0("
+                  function() {
+                    viewer.spin(false);
+                    viewer.render()
+                  }")
+  )
+}
+
+m_button_zoom <- function(id, sel = m_sel(), label = "Zoom!") {
+  m_button(
+    id = id,
+    name = "zoom",
+    label = label,
+    func = paste0(
+      "
+      function() {
+        viewer.zoomTo(", rjson::toJSON(sel), ");
+        viewer.render();
+      }"
+    )
+  )
+}
+
+m_function() {
+  paste0(
+    "
+    function() {
+
+    }"
+  )
+}
+
+m_button_make <- function(x) {
+  print(environment(x))
+}
+
+m_button_make(c())
+
+m_glimpse("4ozs")
+
+m_button_style(sel = m_sel(resn = c("ALA", "GLY")),
+               style = m_style_stick(color = "red"),
+               label = "Sticks") %>%
+  m_button_style(style = m_style_cartoon(color = "spectrum"),
+                 label = "Cartoon") %>%
+  # m_button_style2() %>%
+  m_button_spin(speed = 0.2) %>%
+  m_button_spin(speed = -0.2, label = "Reverse!") %>%
+  m_button_stop_spin() %>%
+  m_button_zoom(m_sel(resi = 1:30)) %>%
+  m_button_zoom(label = "Zoom out!")
